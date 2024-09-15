8693847e6db61fbf7fe523b6e690640058f41de4
Riddhisha Chitwadgi <riddhisha.1si18im008@gmail.com>
2024-09-14 21:47:17 -0400

    Delete Superstore.sql

--- Superstore.sql
+++
@@ -1,5 +0,0 @@
-SELECT "Order Date", 
-       "Sales", 
-       AVG("Sales") OVER (ORDER BY "Order Date" 
-                          ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg_3_day
-FROM superstore_data;
